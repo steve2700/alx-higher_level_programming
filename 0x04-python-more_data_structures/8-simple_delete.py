e49360e84a3add50dfbf8d124ab36036618d4c8f
steve2700 <stevnyaruwa@gmail.com>
2022-12-09 01:49:52 -0800

    ss

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,4 @@ def simple_delete(a_dictionary, key=""):
     if(a_dictionary):
         if(a_dictionary.get(key)):
             del a_dictionary[key]
-            return(a_dictionary)
-
+    return(a_dictionary)
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,1 +1,1 @@
-hello World
+helo World

679df9357490fb6c16c32f66091a7a8d43faa6ac
steve2700 <steve2700@github.com>
2023-01-09 13:28:26 -0800

    base geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""basegeometry"""
+
+
+class BaseGeometry:
+    """instance of geometry class"""
+    def __init__(self):
+        """initialize class"""
+        pass
+
+
+
+
+
+

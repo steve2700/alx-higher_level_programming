09869e724ec59e6dc6b2a0ab350e9a10b1eba34c
steve2700 <steve2700@github.com>
2023-01-10 13:04:12 -0800

    codeine

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""This modules defines a file-appending function"""
+"""This module defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of the UTB text file"""
-    with open(filename, "a", encoding=utf-8") as f
-    return f.write(text)
+    """Appends a string to the end of a UTF8 text file
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)

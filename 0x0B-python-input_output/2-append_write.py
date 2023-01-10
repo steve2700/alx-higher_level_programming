14cfa47e0ce855cb6495062891351181283d1768
steve2700 <steve2700@github.com>
2023-01-10 12:56:39 -0800

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This modules defines a file-appending function"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to the end of the UTB text file"""
+    with open(filename, "a", encoding=utf-8") as f
+    return f.write(text)

5ef7e9de8ace5156b68e63b3f5eaa17d876025f0
steve2700 <steve2700@github.com>
2023-01-10 12:40:23 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""return the number of lines"""
+
+"""This module defines a file-writing function."""
+
 
 
 def write_file(filename="", text=""):
-    """number of lines"""
-    lines = 0
-    with open(filename, mode='r', encoding='utf-8') as f:
-        for line in f:
-            lines +=1
-    return lines
+    """Writes a string to a UTF8 text file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)

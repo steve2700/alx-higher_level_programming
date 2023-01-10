62d396946181a068cfd0189abfe810149f2ad6b5
steve2700 <steve2700@github.com>
2023-01-10 12:14:58 -0800

    a

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """reads a file"""
-    with open(filename, mode= 'r',encoding='utf-8') as f:
+    with open(filename, mode='r', encoding='utf-8') as f:
         for line in f:
             print(line, end="")

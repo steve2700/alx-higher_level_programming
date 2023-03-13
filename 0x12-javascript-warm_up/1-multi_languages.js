3a38e2409087988caa1a6b638d3783797d156f67
steve2700 <stevnyaruwa@gmail.com>
2023-03-13 20:57:19 +0200

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-// Script that prints C is fun,Python is cool and JavaScript is amazing.
-
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+let words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < words.length; i++) {
+  console.log(words[i]);
+}

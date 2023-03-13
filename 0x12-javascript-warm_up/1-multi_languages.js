0b6487fdfd4d31736652a2dd26bce884e6d4f559
steve2700 <stevnyaruwa@gmail.com>
2023-03-13 21:01:09 +0200

    chatgpt

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-let words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < words.length; i++) {
-  console.log(words[i]);
-}
+// Script that prints 3 lines
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'JavaScript is amazing';
+
+console.log(c);
+console.log(python);
+console.log(js);
+

af618d5616488d0341f0ad4f8ec278a5951bc892
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-03-13 17:20:59 +0200

    Update 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-<<<<<<< HEAD
 // Script that prints "JavaScript is amazing".
 
-=======
->>>>>>> 410e22c0d398e943a6b63f5cfd27b16a9b33b376
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
 

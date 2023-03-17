55bc8f8608d89949b6135ec0d71dfd2a78bed5ea
steve2700 <steve2700@github.com>
2023-03-17 08:12:42 -0700

    k

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""
-This script takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
-"""
+# gets all states via python yee boi with your own state
 
-import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
+def main(args):
+    # gets all state stuff by N
+    if len(args) != 5:
+        raise Exception("need 4 arguments!")
+    db = MySQLdb.connect(host='localhost',
+                         user=args[1],
+                         passwd=args[2],
+                         db=args[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
-    rows = cur.fetchall()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC"
+        .format(args[4]))
+    states = cur.fetchall()
+    for state in states:
+        if state[1] == args[4]:
+            print(state)
 
-    for row in rows:
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    main(sys.argv)

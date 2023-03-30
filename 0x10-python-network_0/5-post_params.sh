7c06f31983c880ba0ea01cecaf257a4123baa0f9
steve2700 <steve2700@github.com>
2023-03-30 07:59:04 -0700

    im done

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+#Bash script that takes in a URL
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1" 
 
 

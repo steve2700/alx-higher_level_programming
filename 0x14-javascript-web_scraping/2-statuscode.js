91dc7e3244e23d95b74440796d141c6be097b09e
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-06-29 12:58:36 +0200

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-
-if (process.argv.length > 2) {
-  request(process.argv[2], (err, res, body) => {
-    if (err) console.log(err);
-    console.log('code:', res.statusCode);
-  });
-}
